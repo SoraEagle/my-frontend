4d0ac09f1e4363bc85dbe945fec771f8b44eb0f8
Matthew Phillips <phillips.matthew829@gmail.com>
2022-05-05 15:24:29 -0500

    Add submit form to Trips

--- src/components/Trip.js
+++ src/components/Trip.js
@@ -1,36 +1,28 @@
-import React, {useContext} from "react";
-import {TripsContext} from "./context/MyTrips.js";
+function Trip({trip, onDeleteTrip}){
+    
+    function deleteTrip(){
+        fetch(`http://localhost:9292/trips/${trip.id}`, { // DELETE fetch request.
+        method: "DELETE",
+    })
+    .then((r) => r.json())
+    .then(() => onDeleteTrip(trip)); // Invoke the onDeleteItem function with this fetch request.
+    }
 
-// Add CRUD capability in order to create, read, update, and delete trips (and their items)...
-
-function Trip(){
-    const {trip, setTrip} = useContext(TripsContext);
-
-    function createTrip(){
+    function updateTrip(){
         // 
     }
 
-    // function deleteTrip(){
-    //     fetch(`http://localhost:9292/item/${trip.id}`, { // DELETE fetch request.
-    //     method: "DELETE",
-    // })
-    // .then((r) => r.json())
-    // .then(() => onDeleteTrip(trip));
-    // }
-
-    // return(
-    //     <div>
-    //         <div id="item" style={{
-    //         borderBottom: "2px solid black",
-    //         paddingBottom: "10px",
-    //         marginBottom: "12px"
-    //       }}>
-    //           <p>#{trip.name}</p>
-    //           <button>Update</button>
-    //           <button onClick={deleteTrip}>Delete</button>
-    //     </div>
-    //     </div>
-    // );
+    return(
+        <div id="trip" style={{
+            borderBottom: "2px solid black",
+            paddingBottom: "10px",
+            marginBottom: "12px"
+          }}>
+              <p>#{trip.name}</p>
+              <button>Update</button>
+              <button onClick={deleteTrip}>Delete</button>
+        </div>
+    );
 }
 
 export default Trip;
--- src/components/Trips.js
+++ src/components/Trips.js
@@ -1,34 +1,64 @@
 import React, {useContext, useState} from "react";
+import {TripsContext} from "./context/Trips";
 import Trip from "./Trip";
 
 function Trips(){
+    const {trips, setTrips} = useContext(TripsContext);
     const [name, setName] = useState('');
 
     function handleNameChange(e){
-        setName(e.target.value);
-      }
+      setName(e.target.value);
+    }
 
-      function handleSubmit(e){
-        e.preventDefault();
-        // fetch("http://localhost:9292/items",
-        // )
-      }
-    return(
+    // function handleTripChange(e){
+    //   setTrip(e.target.value);
+    // }
+
+    function handleSubmit(e){
+      e.preventDefault();
+      // fetch("http://localhost:9292/trips",
+      // )
+    }
+
+    function onDeleteTrip(deletedTrip){
+      const updatedTrips = trips.filter((trip) => trip.id !== deletedTrip.id);
+      setTrips(updatedTrips);
+    }
+    
+    if(Trip.length === 0) return(
         <div>
-            <h2 style={{
+          <p>You have no items for your trips!</p>
+          <img src="https://miro.medium.com/max/1280/1*-Nr0OP_Nu7b2NPrcgJ1SuA.png" alt="null"/>
+          </div>);
+          else console.log("Trips: ", Trips);
+
+          return(
+            <div id="trips">
+              <h2 style={{
                 borderBottom: "2px solid black"
               }}>
-                 Make a Trip:
+                Make a Trip:
               </h2>
 
               <form onSubmit={handleSubmit}>
                 <div><label><b>Name: 
                   <input type="text" name="name" placeholder="name" onChange={handleNameChange} />
                 </b></label></div>
-                <button type="submit">Submit New Trip</button>
+                <button type="submit">Submit Trip</button>
               </form>
-        </div>
-    );
+
+              <h2 style={{
+                borderBottom: "2px solid black",
+                marginBottom: "12px",
+                marginTop: "12px",
+              }}>
+                My Trips:
+              </h2>
+              {trips.map((trip) => {
+                return <h5 key={trip.id}><Trip trip={trip} onDeleteTrip={onDeleteTrip} /></h5>
+              })}
+            </div> 
+          );
 }
 
 export default Trips;
