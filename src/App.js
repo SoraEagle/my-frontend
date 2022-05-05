befb63739c325a8c46f0467fc8f65ef6774c7274
Matthew Phillips <phillips.matthew829@gmail.com>
2022-05-05 13:17:01 -0500

    Edit App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import React from "react";
+import {Routes, Route, Link, Switch} from "react-router-dom";
+// import {TripsProvider} from './components/context/MyTrips';
+// import {ItemsProvider} from './components/context/MyItems';
+// import NavBar from './components/NavBar';
+// import Home from './components/Home';
+// import Trips from './components/Trips';
+// import Items from './components/Items';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+function App(){
+  // Have a Home page?
+  // Add useStates?
+
+  return(
+    <div className="App"
+    style={{
+      backgroundColor: "aqua",
+      height: "100vh",
+      textAlign: "center"
+    }}>
+
+      <footer id="footer" style={{
+          backgroundColor: "coral",
+          borderBottom: "2px solid black",
+          marginBottom: "12px",
+          marginTop: "12px",
+          borderTop: "2px solid black",
+          paddingTop: "10px",
+          textAlign: "center"
+        }}>
+          <p>@2022 Ruby Faker Inc.</p>
+          <p>All rights reserved.</p>
+        </footer>
     </div>
   );
 }
