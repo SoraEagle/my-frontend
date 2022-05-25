02a705cd352fbabde43ed6e6bd332a942c863e7e
Matthew Phillips <phillips.matthew829@gmail.com>
2022-05-25 13:44:03 -0500

    Align lines in Trips.js

--- src/components/Trips.js
+++ src/components/Trips.js
@@ -1,34 +1,34 @@
-import React, {useContext} from "react";
-import {TripsContext} from "./context/myTrips";
+import React, { useContext } from "react";
+import { TripsContext } from "./context/myTrips";
 import Trip from "./Trip";
 import NewTrip from "./NewTrip";
 
-function Trips(){
-    const {trips, setTrips} = useContext(TripsContext);
+function Trips() {
+  const { trips, setTrips } = useContext(TripsContext);
 
-    return (
-      <div id="trips">
-        <h2 style={{borderBottom: "2px solid black"}}>
-                Make a Trip:
-              </h2>
-              <NewTrip trips={trips} setTrips={setTrips} />
+  return (
+    <div id="trips">
+      <h2 style={{ borderBottom: "2px solid black" }}>
+        Make a Trip:
+      </h2>
+      <NewTrip trips={trips} setTrips={setTrips} />
 
-              <h2 style={{
-                borderBottom: "2px solid black",
-                marginBottom: "12px",
-                marginTop: "12px",
-              }}>
-                My Trips:
-              </h2>
+      <h2 style={{
+        borderBottom: "2px solid black",
+        marginBottom: "12px",
+        marginTop: "12px",
+      }}>
+        My Trips:
+      </h2>
 
-              {(trips.length === 0)? (
-                <h1>You have no trips!</h1>
-              ) : (
-                <div>
-                  {trips.map((trip) => {return <h5 key={trip.id}><Trip trip={trip} /></h5>})}
-                </div>)}
-      </div>
-    );
+      {(trips.length === 0) ? (
+        <h1>You have no trips!</h1>
+      ) : (
+        <div>
+          {trips.map((trip) => { return <h5 key={trip.id}><Trip trip={trip} /></h5> })}
+        </div>)}
+    </div>
+  );
 }
 
 export default Trips;
